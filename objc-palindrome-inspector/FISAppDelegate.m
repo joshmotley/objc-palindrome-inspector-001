85e4076df098c81b12e97cbfa4b5b197039d3900
joshmotley <joshkmotley@gmail.com>
2016-01-19 19:35:23 -0500

    pass tests with palindrome methods

--- objc-palindrome-inspector/FISAppDelegate.m
+++ objc-palindrome-inspector/FISAppDelegate.m
@@ -15,4 +15,41 @@
  
  */
 
+-(NSString *)stringByReversingString:(NSString *)string{
+    
+    NSString *result = @"";
+    for (NSUInteger i = [string length]; i>0; i--) {
+        unichar c = [string characterAtIndex:i-1];
+        result= [result stringByAppendingString:[NSString stringWithFormat:@"%C",c]];
+        
+    }
+    
+    
+    return result;
+}
+
+-(BOOL)stringIsPalindrome:(NSString *)string{
+    
+    NSArray *punctuations = @[@".", @",", @"!", @"?", @":", @";"];
+    
+    NSString *withoutPunctuation = [string copy];
+    for (NSUInteger i = 0; i<[punctuations count]; i++) {
+        NSString *punctuation = punctuations[i];
+        withoutPunctuation = [withoutPunctuation stringByReplacingOccurrencesOfString:punctuation withString:@""];
+        
+    }
+    
+    
+    NSString *spaceless = [withoutPunctuation stringByReplacingOccurrencesOfString:@" " withString:@""];
+    NSString *lowercase = [spaceless lowercaseString];
+    NSString *reversed = [self stringByReversingString:lowercase];
+    
+    
+    
+    BOOL isEqualToReversed = [lowercase isEqualToString:reversed];
+    
+    return isEqualToReversed;
+    
+}
+
 @end
